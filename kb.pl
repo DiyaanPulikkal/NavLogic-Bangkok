c6ca3519261fab3c04a4773c62b3950e20c57655
Diyaan Pulikkal <66010991@kmitl.ac.th>
2026-02-11 10:48:27 +0700

    Update kb.pl

---
+++ kb.pl
@@ -0,0 +1,87 @@
+/*
+--------------------------------------------------
+BTS Sukhumvit Line
+--------------------------------------------------
+*/
+station('Ratchathevi (N1)', 'bts_sukhumvit').
+station('Phaya Thai (N2)', 'bts_sukhumvit').
+station('Victory Monument (N3)', 'bts_sukhumvit').
+station('Sanam Pao (N4)', 'bts_sukhumvit').
+station('Ari (N5)', 'bts_sukhumvit').
+station('Sena Ruam (N6)', 'bts_sukhumvit').
+station('Saphan Khwai (N7)', 'bts_sukhumvit').
+station('Mo Chit (N8)', 'bts_sukhumvit').
+station('Ha Yaek Lat Phrao (N9)', 'bts_sukhumvit').
+station('Phahon Yothin 24 (N10)', 'bts_sukhumvit').
+station('Ratchayothin (N11)', 'bts_sukhumvit').
+station('Sena Nikhom (N12)', 'bts_sukhumvit').
+station('Kasetsart University (N13)', 'bts_sukhumvit').
+station('Royal Forest Department (N14)', 'bts_sukhumvit').
+station('Bang Bua (N15)', 'bts_sukhumvit').
+station('11th Infantry Regiment (N16)', 'bts_sukhumvit').
+station('Wat Phra Sri Mahathat (N17)', 'bts_sukhumvit').
+station('Phahon Yothin 59 (N18)', 'bts_sukhumvit').
+station('Sai Yud (N19)', 'bts_sukhumvit').
+station('Saphan Mai (N20)', 'bts_sukhumvit').
+station('Bhumibol Adulyadej Hospital (N21)', 'bts_sukhumvit').
+station('Royal Thai Air Force Museam (N22)', 'bts_sukhumvit').
+station('Yaek Kor Por Aor (N23)', 'bts_sukhumvit').
+station('Khu Khot (N24)', 'bts_sukhumvit').
+
+station('Chit Lom (E1)', 'bts_sukhumvit').
+station('Phloen Chit (E2)', 'bts_sukhumvit').
+station('Nana (E3)', 'bts_sukhumvit').
+station('Asok (E4)', 'bts_sukhumvit').
+station('Phrom Phong (E5)', 'bts_sukhumvit').
+station('Thong Lo (E6)', 'bts_sukhumvit').
+station('Ekkamai (E7)', 'bts_sukhumvit').
+station('Phra Khanong (E8)', 'bts_sukhumvit').
+station('On Nut (E9)', 'bts_sukhumvit').
+station('Bang Chak (E10)', 'bts_sukhumvit').
+station('Punnawithi (E11)', 'bts_sukhumvit').
+station('Udom Suk (E12)', 'bts_sukhumvit').
+station('Bangna (E13)', 'bts_sukhumvit').
+station('Bearing (E14)', 'bts_sukhumvit').
+station('Samrong (E15)', 'bts_sukhumvit').
+station('Pu Chao (E16)', 'bts_sukhumvit').
+station('Chang Erawan (E17)', 'bts_sukhumvit').
+station('Royal Thai Naval Academy (E18)', 'bts_sukhumvit').
+station('Pak Nam (E19)', 'bts_sukhumvit').
+station('Srinagarindra (E20)', 'bts_sukhumvit').
+station('Phraek Sa (E21)', 'bts_sukhumvit').
+station('Sai Luat (E22)', 'bts_sukhumvit').
+station('Kheha (E23)', 'bts_sukhumvit').
+
+/*
+--------------------------------------------------
+BTS Silom Line
+--------------------------------------------------
+*/
+station('National Stadium (W1)', 'bts_silom').
+
+station('Ratchadamri (S1)', 'bts_silom').
+station('Saladaeng (S2)', 'bts_silom').
+station('Chong Nonsi (S3)', 'bts_silom').
+station('Saint Louis (S4)', 'bts_silom').
+station('Surasak (S5)', 'bts_silom').
+station('Saphan Taksin (S6)', 'bts_silom').
+station('Krung Thon Buri (S7)', 'bts_silom').
+station('Wongwian Yai (S8)', 'bts_silom').
+station('Pho Nimit (S9)', 'bts_silom').
+station('Talat Phlu (S10)', 'bts_silom').
+station('Wutthakat (S11)', 'bts_silom').
+station('Bang Wa (S12)', 'bts_silom').
+
+/*
+--------------------------------------------------
+BTS Center
+--------------------------------------------------
+*/
+
+station('Siam (CEN)', 'bts_center').
+
+/*
+--------------------------------------------------
+Gold Line
+--------------------------------------------------
+*/
