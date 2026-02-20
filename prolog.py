e1b3deeaca515beea821ba829f4e1430b6b1f61e
Diyaan Pulikkal <pulikkal.diyaan@gmail.com>
2026-02-20 20:43:34 +0700

    set up prolog interface

--- prolog.py
+++ prolog.py
@@ -1,3 +1,19 @@
+import re
+from pyswip import Prolog
+
 class PrologInterface:
     def __init__(self):
-        pass
+        self.prolog = Prolog()
+        self.prolog.consult("knowledge_base.pl")
+    
+    def is_valid_query(self, query):
+        pattern = r'^[a-zA-Z_][a-zA-Z0-9_]*\((.*)\)\.$'
+        return re.match(pattern, query) is not None
+    
+    def query(self, query):
+        if self.is_valid_query(query):
+            try:
+                result = list(self.prolog.query(query[:-1]))
+                return result
+            except Exception as e:
+                return f"Error executing query: {e}"
